1dcdb39d50ad1cdb881dd9dc0c7644369ece4d56
MAXONCHIK236 <m-garzhaikin@bk.ru>
2023-11-23 21:42:08 +0300

    last

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import styles from   "./BurgerMenu.module.scss"
-import Burger from "../../assents/header_img/headerBurgerMenu.svg"
+import styles from "./BurgerMenu.module.scss";
+import Burger from "../../assents/header_img/headerBurgerMenu.svg";
 import TranslationDropdown from "../TranslationDrop/TranslationDropdown";
+import { useTranslation } from "react-i18next";
 const BurgerMenu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -11,25 +12,33 @@ const BurgerMenu = () => {
 
   return (
     <div className={styles.burger_menu}>
-      <div className={`${styles.menu_button} ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
-       <img src={Burger} alt="burger" />
+      <div
+        className={`${styles.menu_button} ${menuOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <img src={Burger} alt="burger" />
       </div>
       {menuOpen && (
         <div className={styles.menu_items}>
           <ul>
-          <TranslationDropdown/>
-            <li> <a href="/">Главная</a></li>
-            <li> <a href="/AboutUs"> О нас </a></li>
-            <li> <a href="/ServicePersonal">Для персонала</a></li>
-            <li>  <a href="/ServiceEmployer">Для работодателей </a></li>
-            <li> <a href="/Contact">Контакты</a></li>
-          
+            <TranslationDropdown />
+            <li>
+              <a href="/">{t("header.home")}</a>
+            </li>
+            <li>
+              <a href="/AboutUs"> {t("header.about")} </a>
+            </li>
+            <li>
+              <a href="/ServicePersonal">{t("header.forpersonal")}</a>
+            </li>
+            <li>
+              <a href="/ServiceEmployer">{t("header.foremployer")} </a>
+            </li>
+            <li>
+              <a href="/Contact">{t("header.contact")}</a>
+            </li>
           </ul>
         </div>
-           
-           
-       
-           
       )}
     </div>
   );
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./main.module.scss";
-import image from "../../assents/personal/bg3.svg"
+import image from "../../assents/personal/main3.jpg"
 import { useTranslation } from "react-i18next";
 import rightArrow from "../../assents/cursor_decoration/right.svg";
 import "../../i18n.js";
